92e58d74548838ddb9582b611d679acfad6024a0
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2023-06-20 07:36:16 -0700

    [ADD] Terminada la logica de match del pokemon oculto y la respuesta, tambien se agregan mensajes para dar feedback al usuario de sus acciones y un boton de reinicio

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="options-container">
         <ul>
-            <li v-for="pokemon in pokemons" :key="pokemon.id">
+            <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selection', pokemon.id)">
+                
                 {{ pokemon.names }}
             </li>
         </ul>
--- src/pages/pokemonPage.vue
+++ src/pages/pokemonPage.vue
@@ -5,7 +5,13 @@
     <div v-else="pokemon">
         <h1>¿Quién es este pokémon?</h1>
         <PokemonImage :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
-        <PokemonOptions :pokemons="pokemonArr"/>
+        <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer"/>
+
+        <h2> {{ message }}</h2>
+
+        <button v-if="showPokemon" @click="newGame">
+            Nuevo juego
+        </button>
     </div>
 </template>
 
@@ -23,7 +29,8 @@ export default {
         return {
             pokemonArr: [],
             pokemon: null,
-            showPokemon: false
+            showPokemon: false,
+            message: ''
         }
     },
     methods: {
@@ -33,6 +40,21 @@ export default {
             const rndInt = Math.floor(Math.random() * 4)
             this.pokemon = this.pokemonArr[rndInt]
         },
+        checkAnswer(pokemonID) {
+            if (pokemonID === this.pokemon.id){
+                this.showPokemon = true
+                this.message = 'You Catch it.'
+            } else {
+                this.message = 'Try again.'
+            }
+        },
+        newGame(){
+            this.mixPokemonArr(),
+            this.showPokemon = false
+            this.message = '',
+            this.pokemonArr = [],
+            this.pokemon = null
+        }
     },
     mounted() {
         this.mixPokemonArr()
